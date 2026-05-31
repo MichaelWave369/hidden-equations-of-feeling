9ff1f3af8f9b3b94055d8c069ce84a7f2a583045
Michael Hughes <microneesia79@gmail.com>
2026-05-31 10:16:52 -0700

    Import example gallery styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles/tokens.css';
 import './styles/copy-tools.css';
 import './styles/compare-mode.css';
+import './styles/example-gallery.css';
 import './styles/print.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
